a569aa529fa862ba732febddb885836c15c6aa26
DigitalDieter <digitaldieter@MacBook-Pro.local>
2019-05-22 23:46:37 +0200

    Initial commit

---
+++ find_save_https.py
@@ -0,0 +1,38 @@
+import asyncio
+import argparse
+from proxybroker import Broker
+
+parser = argparse.ArgumentParser(description='Find Proxy Server')
+parser.add_argument('-n', '--n_results', type=int, default=100,
+                    help='number of proxy server')
+
+args = parser.parse_args()
+number = args.n_results
+
+filename = 'source/' + str(number) + '_https_proxies.txt'
+
+async def save(proxies, filename):
+    count = 0
+    with open(filename, 'w') as f:
+        while True:
+            proxy = await proxies.get()
+            if proxy is None:
+                break
+            # proto = 'https' if 'HTTPS' in proxy.types else 'http'
+            row = '%s:%d,\n' % (proxy.host, proxy.port)
+            f.write(row)
+            count = count + 1
+            print(count, row)
+
+
+def main():
+    proxies = asyncio.Queue()
+    broker = Broker(proxies)
+    tasks = asyncio.gather(broker.find(types=['HTTPS'], limit=number),
+                           save(proxies, filename=filename))
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(tasks)
+
+
+if __name__ == '__main__':
+    main()
---
+++ find_save_socks5.py
@@ -0,0 +1,38 @@
+import asyncio
+import argparse
+from proxybroker import Broker
+
+parser = argparse.ArgumentParser(description='Find Proxy Server')
+parser.add_argument('-n', '--n_results', type=int, default=50,
+                    help='number of proxy server')
+
+args = parser.parse_args()
+number = args.n_results
+filename = 'source/' + str(number) + '_socks5_proxies.txt'
+
+
+async def save(proxies, filename):
+    count = 0
+    with open(filename, 'w') as f:
+        while True:
+            proxy = await proxies.get()
+            if proxy is None:
+                break
+            # proto = 'socks5' if 'SOCKS5' in proxy.types
+            row = '%s:%d,\n' % (proxy.host, proxy.port)
+            f.write(row)
+            count = count + 1
+            print(count, row)
+
+
+def main():
+    proxies = asyncio.Queue()
+    broker = Broker(proxies)
+    tasks = asyncio.gather(broker.find(types=['SOCKS5'], limit=number),
+                           save(proxies, filename=filename))
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(tasks)
+
+
+if __name__ == '__main__':
+    main()
