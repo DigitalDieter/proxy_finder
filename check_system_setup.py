9fd295f5a14f15a236a449ace62358cbf4765b66
Michael <m.mueller444@gmail.com>
2019-07-02 18:50:16 +0200

    Code updated

--- check_system_setup.py
+++ check_system_setup.py
@@ -1,6 +1,13 @@
 import requests
 import pandas
 import proxybroker
+import os
+
+
+# create file structure for training data
+if not os.path.exists('datasets/'):
+    os.makedirs("source")
+
 
 print("package versions:")
 print("requests:",requests.__version__)
