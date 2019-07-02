a4a73c21c49f317fad1c089b26f291f8501dcae9
Michael <m.mueller444@gmail.com>
2019-07-02 18:26:52 +0200

    check_system_setup.py added

---
+++ check_system_setup.py
@@ -0,0 +1,10 @@
+import requests
+import pandas
+import proxybroker
+
+print("package versions:")
+print("requests:",requests.__version__)
+print("pandas:",pandas.__version__)
+print("proxybroker:",proxybroker.__version__)
+
+
