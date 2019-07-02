8dca1d11aa1d929c2b4057f95a68d680505bbcf2
Michael <m.mueller444@gmail.com>
2019-07-03 00:44:48 +0200

    check_system_setup.py updated

--- check_system_setup.py
+++ check_system_setup.py
@@ -5,9 +5,11 @@ import os
 
 
 # create file structure for training data
-if not os.path.exists('datasets/'):
+if not os.path.exists('source/'):
+    print("create folder source")
     os.makedirs("source")
-
+else:
+    print("folder source already existing")
 
 print("package versions:")
 print("requests:",requests.__version__)
